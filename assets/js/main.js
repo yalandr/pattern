7d0822bd3e0a686a2cfb2a8c51f245095d8ed355
yalandr <52127093+yalandr@users.noreply.github.com>
2022-05-13 15:35:08 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+// NAVIGATION
+const scrollToElem = (elem) => {
+    document.querySelector(elem).scrollIntoView({behavior:"smooth"})
+}
+
+// ANIMATION ON SCROLL
+let animatedItems = document.querySelectorAll('.animated');
+
+if (animatedItems.length) {
+    function fadeInOnScroll() {
+        for (i = 0; i < animatedItems.length; i++) {
+            let animatedItem = animatedItems[i];
+            let animatedItemHeight = animatedItem.offsetHeight;
+            let animatedItemOffset = offset(animatedItem).top;
+            let animationStart = 2;
+
+            let animatedItemPoint = window.innerHeight - animatedItemHeight / animationStart;
+
+            if (animatedItemHeight > window.innerHeight) {
+                animatedItemPoint = window.innerHeight - window.innerHeight / animationStart;
+            }
+
+            if ((pageYOffset > animatedItemOffset - animatedItemPoint) && pageYOffset < (animatedItemOffset + animatedItemHeight)) {
+                animatedItem.classList.add('fade-in');
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+}
+
+window.addEventListener('scroll', fadeInOnScroll);
+
+// CURRENT YEAR
+document.querySelector('.current-year').innerHTML = new Date().getFullYear();
+
+// Form Submission
+let nameValue = document.querySelector('.name');
+let lastnameValue = document.querySelector('.lastname');
+let emailValue = document.querySelector('.email');
+let phoneValue = document.querySelector('.phone');
+let requiredFields = document.querySelector('.required-fields');
+
+const formSubmission = () => {
+    if (nameValue.value != '' && lastnameValue.value != '' && emailValue.value != '' && phoneValue.value != '') {
+        window.location.href = 'thankyou.html';
+    } else {
+        requiredFields.classList.add('visible');
+    }
+}
+
+const inputFields = document.querySelectorAll('.name, .lastname, .email, .phone');
+for (let inputItem of inputFields) {
+    inputItem.addEventListener('focus', function() {
+        if (requiredFields.classList.contains('visible')) {
+            requiredFields.classList.remove('visible');
+        }
+    });
+}
